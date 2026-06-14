d16d3d5a3a3e2586e7e137d9bb991a5c09ca2640
nirshithadarji29-cmd <nirshithadarji29@gmail.com>
2026-06-14 11:48:55 +0530

    Update requirements.txt.py

--- requirements.txt.py
+++ requirements.txt.py
@@ -1,12 +1,5 @@
-import pandas
-import plotly
-import sqlalchemy
-import streamlit
-from pyarrow.lib import binary
-from sqlalchemy.dialects.postgresql import psycopg2
-
-# streamlit
-# pandas
-# plotly
-# sqlalchemy
-# psycopg2-binary
+streamlit
+pandas
+plotly
+sqlalchemy
+psycopg2-binary
